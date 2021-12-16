8a3259263d929ea93f8064e835030b3bb72b6b06
MatheusSantosz <matheus.santos.ciencia@gmail.com>
2021-12-16 11:00:28 -0300

    first commit

---
+++ cypress/integration/app-init.spec.js
@@ -0,0 +1,9 @@
+/// <reference types = "cypress"/>
+
+describe('App initialization', () => {
+  it('Displays todos from API on load', () => {
+    cy.seedAndVisit();
+
+    cy.get('.task-wrapper').should('have.length', 4);
+  });
+});
